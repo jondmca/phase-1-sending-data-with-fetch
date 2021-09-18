87d80d3fd093121d0f3b564e8a9530c316797838
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-09-17 20:38:16 -0500

    commit tree d91bfc62c26f1c7c1a635d9e1a630399f7d41b7a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+    .then(res => res.json())
+    .then(data => renderUser(data))
+    .catch(function (error){
+        console.log(error)
+        alert("Unauthorized Access")
+        document.body.append(error.message)
+    })
+}
+
+function renderUser(data){
+    console.log(data.id)
+    document.body.append(data.id)
+}
+
